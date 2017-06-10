d1bbcc1bb4eb05567e965ddec2f0f3de2c1ba39b
equipeazul <barbosanj@hotmail.com>
2017-06-10 16:35:22 -0300

    aewwww

--- Vendas/src/vendas/Gui/TelaPrincipal.java
+++ Vendas/src/vendas/Gui/TelaPrincipal.java
@@ -150,7 +150,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
+        PedidoCadastro.abrir(jDesktopPane1);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
